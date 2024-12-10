0755706acaabf1057f08b4eafe68997e84e837b5
DarrylSeshie <la226963@student.helha.be>
2024-12-10 02:47:36 +0100

    form add article, update article + manage user via keyclok

--- FrontEnd/Journal-App/src/app/manage-article/manage-article.component.ts
+++ FrontEnd/Journal-App/src/app/manage-article/manage-article.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-manage-article',
   templateUrl: './manage-article.component.html',
-  styleUrl: './manage-article.component.css'
+  styleUrls: ['./manage-article.component.css'] // Corrigé ici
 })
 export class ManageArticleComponent {
+  
 
+  
+
+
+
+
+  validateFileCount(event: any): void {
+    const files = event.target.files;
+    
+    // Vérifie si plus de 5 fichiers sont sélectionnés
+    if (files.length > 3) {
+      alert("You can upload a maximum of 3 images.");
+      event.target.value = ""; // Réinitialise la sélection
+      return;
+    }
+
+    // Vérifie si tous les fichiers sont des images
+    for (const file of files) {
+      if (!file.type.startsWith('image/')) {
+        alert("Only image files are allowed.");
+        event.target.value = ""; // Réinitialise la sélection
+        return;
+      }
+    }
+
+    // Si tout est correct
+    alert(`${files.length} file(s) selected successfully.`);
+  }
 }
